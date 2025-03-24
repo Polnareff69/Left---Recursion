3cdf9f98c6f673719b049a653af9c03f267fdda5
Polnareff <munozgomezjuanjose69@gmail.com>
2025-03-24 14:29:34 -0500

    Actualizacion del Reame.md

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,9 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        //System.out.println("Ingrese el numero de casos");
         Scanner scanner = new Scanner(System.in);
         int n = Integer.parseInt(scanner.nextLine());
         for(int i = 0; i < n; i++){
-            //System.out.println("Ingrese el numero de producciones");
             int k = Integer.parseInt(scanner.nextLine());
             Map<String, List<String>> grammar = new LinkedHashMap<>();
             for(int j = 0; j  < k; j++){
@@ -21,16 +19,5 @@ public class Main {
             eliminator.eliminateLeftRecursion();
             eliminator.printGrammar();
         }
-
-        /*
-        Map<String, List<String>> grammar = new LinkedHashMap<>();
-        grammar.put("S", Arrays.asList("Aa", "b"));
-        grammar.put("A", Arrays.asList("Ac", "Sd", "m"));
-
-        LeftRecursionEliminator eliminator = new LeftRecursionEliminator(grammar);
-        eliminator.eliminateLeftRecursion();
-        eliminator.printGrammar();
-        */
-
     }
 }
